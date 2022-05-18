6bd2db2cd8c5c6e14c527d9745b28969e4d2e7f2
Sergey Boykov <boykov.sergei@gmail.com>
2022-05-18 18:19:27 +0500

    Modifying the file Startup and Creating the Migrations.

---
+++ MyCompany/Domain/DataManager.cs
@@ -0,0 +1,20 @@
+﻿using MyCompany.Domain.Repositories.Abstract;
+
+namespace MyCompany.Domain
+{
+    /// <summary>
+    /// Класс-помощник для управления текстовыми полями и услугами,
+    /// а также операциями над ними.
+    /// </summary>
+    public class DataManager
+    {
+        public ITextFieldsRepository TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepository textFieldsRepository, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepository;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
--- MyCompany/Startup.cs
+++ MyCompany/Startup.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCompany.Domain;
+using MyCompany.Domain.Repositories.Abstract;
+using MyCompany.Domain.Repositories.EntityFramework;
 using MyCompany.Service;
 using System;
 using System.Collections.Generic;
@@ -23,7 +28,40 @@ namespace MyCompany
         {
             // Подключаем класс Config с инфо из appsettings.json
             Configuration.Bind("Project", new Config());
-            
+
+            // Подключаем нужный функционал приложения в качестве сервисов.
+            // Связываем интерфейс с реализацией этого интерфейса.
+            // В любой момент можем заменить на другую реализацию (будет тогда
+            // другой DbContext, другой Provider).
+            // Создаётся по запросу.
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            // Подключаем контекс БД.
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            // Настраиваем Identity систему.
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true; // Подтверждение e-mail письмом.
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            // Настраиваем authentication cookie.
+            services.ConfigureApplicationCookie(option =>
+            {
+                option.Cookie.Name = "myCompanyAuth";
+                option.Cookie.HttpOnly = true; // не доступны на клиентской стороне.
+                option.LoginPath = "/account/login";
+                option.AccessDeniedPath = "/account/accessdenied";
+                option.SlidingExpiration = true;
+            });
+
             // Добавляем поддержку контроллеров и представлений (MVC)
             services.AddControllersWithViews()
                 // выставляем совместимость с asp.net core 3.0,
@@ -47,6 +85,15 @@ namespace MyCompany
             // Подключаем поддержку статичных файлов в приложении (css, js и т.д.)
             // которые в папке wwwroot.
             app.UseStaticFiles();
+
+            // Подключаем систему маршрутизации.
+            app.UseRouting();
+
+            // Подключаем аутентификацию и авторизацию.
+            // !!! ПОСЛЕ системы маршрутизации, но ДО определения маршрутов.
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             // Регистрируем нужные нам маршруты (Endpoints)
             app.UseEndpoints(endpoints =>
