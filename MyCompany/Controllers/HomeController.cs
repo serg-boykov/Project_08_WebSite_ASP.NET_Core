ff8c5b7f2c687eb8006b0723eb77253f167db29d
Sergey Boykov <boykov.sergei@gmail.com>
2022-05-21 15:21:27 +0500

    User Controllers and Views are implemented.

--- MyCompany/Controllers/HomeController.cs
+++ MyCompany/Controllers/HomeController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
 
 namespace MyCompany.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Поле для доступа к нашей доменной модели, к доменным объектам, к нашей БД.
+        /// </summary>
+        private readonly DataManager _dataManager;
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        /// <param name="dataManager"></param>
+        public HomeController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        /// <summary>
+        /// Действие для главной страницы.
+        /// </summary>
+        /// <returns>Представление для главной страницы.</returns>
         public IActionResult Index()
         {
-            return View();
+            return View(_dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        /// <summary>
+        /// Действие для страницы контактов.
+        /// </summary>
+        /// <returns>Представление для страницы контактов.</returns>
+        public IActionResult Contacts()
+        {
+            return View(_dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
---
+++ MyCompany/Controllers/ServicesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+using System;
+
+namespace MyCompany.Controllers
+{
+    public class ServicesController : Controller
+    {
+        /// <summary>
+        /// Поле для доступа к нашей доменной модели, к доменным объектам, к нашей БД.
+        /// </summary>
+        private readonly DataManager _dataManager;
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        /// <param name="dataManager"></param>
+        public ServicesController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        /// <summary>
+        /// Действие для страницы Наши Услуги.
+        /// </summary>
+        /// <param name="id">Идентификатор услуги.</param>
+        /// <returns>Представление для страницы услуг.</returns>
+        public IActionResult Index(Guid id)
+        {
+            // Если передаётся конкретный идентификатор услуги,
+            // то представление этой услуги.
+            if (id != default)
+            {
+                return View("Show", _dataManager.ServiceItems.GetServiceItemById(id));
+            }
+
+            // Если идентификатор услуги НЕ передаётся (пустой), то комбинированная модель.
+
+            // Через ViewBag передаётся текстовое поле PageServices.
+            ViewBag.TextField = _dataManager.TextFields.GetTextFieldByCodeWord("PageServices");
+
+            // Полный перечень услуг.
+            return View(_dataManager.ServiceItems.GetServiceItems());
+        }
+    }
+}
